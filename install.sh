d25008d0a2fb56245a673d529ff8ad95bdfa73f0
Joseph Harper <harperj9@gmail.com>
2018-12-08 15:33:39 -0600

    Add dotfiles

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BACKUPDIR=~/dotfiles_backup
+FILES="bashrc vimrc alias gitconfig inputrc profile"    # list of files/folders to symlink in homedir
+
+# create dotfiles_backup in homedir
+echo "Creating $BACKUPDIR for backup of any existing dotfiles in ~"
+mkdir -p $BACKUPDIR
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $BASEDIR directory"
+cd $BASEDIR
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $FILES; do
+	echo "Moving any existing dotfiles from ~ to $BACKUPDIR"
+	mv ~/.$file ~/dotfiles_backup
+	echo "Creating symlink to $file in home directory."
+	ln -s $BASEDIR/$file ~/.$file
+done
---
+++ vimrc
@@ -0,0 +1,6 @@
+set nocompatible
+filetype plugin on
+syntax on
+
+" vimwiki/vimwiki
+let g:vimwiki_list = [{'path': '/mnt/c/Users/harpe/Documents/docs/wiki', 'syntax': 'markdown', 'ext': '.md'}]
